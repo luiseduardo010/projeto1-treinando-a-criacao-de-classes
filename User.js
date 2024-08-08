1b5fe24e8178f32e8366fcf00e12b5100e512799
Luis Eduardo <119073361+luiseduardo010@users.noreply.github.com>
2024-08-08 20:35:57 -0300

    Add files via upload

---
+++ User.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(fullname, email, password) {
+        this.fullname = fullname
+        this.email = email
+        this.password = password
+    }
+
+    login(email, password) {
+        if (this.email === email & this.password === password) {
+            console.log(`${this.fullname}: Login sucess!`)
+        } else {
+            console.log(`${this.fullname}: Login failed!`)
+        }
+    }
+}
+
+const John = new User(
+    'John',
+    'luiseisabelle@gmail.com',
+    '1234'
+)
+John.login('luiseisabelle@gmail.com', '1234')
+
